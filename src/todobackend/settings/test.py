4f5fce103f3fa0a1b2aeda334bda09ddab7bae2a
Zohar Nyego <zoharngo@gmail.com>
2018-09-08 20:55:39 +0300

    unittest coverage

--- src/todobackend/settings/test.py
+++ src/todobackend/settings/test.py
@@ -1,6 +1,22 @@
 from base import *
 import os
 
+INSTALLED_APPS += ('django_nose', )
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+TEST_OUTPUT_DIR = os.environ.get('TEST_OUTPUT_DIR', '.')
+
+NOSE_ARGS = [
+    '--verbosity=2'         # verbose output
+    ,'--nologcapture'       # don't output log capture
+    ,'--with-coverage'      # activate coverage report
+    ,'--cover-package=todo' # coverage reports will apply to these packages
+    ,'--with-spec'          # spec style tests
+    ,'--spec-color'
+    ,'--with-xunit'         # enable xunit plugin
+    ,'--xunit-file=%s/unittests.xml' % TEST_OUTPUT_DIR  
+    ,'--cover-xml'
+    ,'--cover-xml-file=%s/coverage' % TEST_OUTPUT_DIR        
+]
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
